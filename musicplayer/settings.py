585cf88f4a8ffbb9ded5f4dd40295a2b5b5370b8
Ramadhan Sulthon Alfanie <jannahy313@gmail.com>
2026-09-06 23:23:48 +0700

    Remove old Electron build files

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -14,8 +14,10 @@ import sys
 
 if getattr(sys, 'frozen', False):
     BASE_DIR = Path(sys._MEIPASS)
+    DATA_DIR = Path(r'D:\gabut\Electron\MyDesktopApp\MusicPlayerApp')
 else:
     BASE_DIR = Path(__file__).resolve().parent.parent
+    DATA_DIR = BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -32,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = DATA_DIR / 'media'
 
 
 INSTALLED_APPS = [
@@ -84,7 +86,7 @@ WSGI_APPLICATION = 'musicplayer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATA_DIR / 'db.sqlite3',
     }
 }
 
